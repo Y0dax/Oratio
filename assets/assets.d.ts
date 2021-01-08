70dc425a04beef021f54bd36913eb2f2173cd1d2
y0dax <dave-myers@outlook.com>
2021-01-08 18:54:05 -0500

    styling

--- assets/assets.d.ts
+++ assets/assets.d.ts
@@ -1,14 +1,14 @@
 declare module '*.svg' {
-  const content: any;
+  const content: never;
   export default content;
 }
 
 declare module '*.png' {
-  const content: any;
+  const content: never;
   export default content;
 }
 
 declare module '*.jpg' {
-  const content: any;
+  const content: never;
   export default content;
 }
