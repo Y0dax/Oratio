558ad33e274de0bff43caf15eb421118bab8ac74
y0dax <dave-myers@outlook.com>
2021-01-08 00:42:14 -0500

    added material styles and new icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import icon from '../assets/icon.svg';
+import { makeStyles } from '@material-ui/core/styles';
+import MicOffIcon from '@material-ui/icons/MicOff';
+
+const useStyles = makeStyles({
+  icon: {
+    fontSize: '10rem',
+    // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  },
+  header: {
+    textAlign: 'center',
+  },
+});
 
 const Hello = () => {
+  const classes = useStyles();
   return (
     <div>
       <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
+        <MicOffIcon className={classes.icon} />
       </div>
-      <h1>Project Oratio</h1>
-      {/* <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div> */}
+      <h1 className={classes.header}>Project Oratio</h1>
     </div>
   );
 };
--- src/main.dev.ts
+++ src/main.dev.ts
@@ -71,7 +71,7 @@ const createWindow = async () => {
     show: false,
     width: 1024,
     height: 728,
-    icon: getAssetPath('icon.png'),
+    icon: getAssetPath('icon1.ico'),
     webPreferences: {
       nodeIntegration: true,
     },
