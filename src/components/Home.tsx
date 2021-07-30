772207efc8a1a5fcd8f44221a29017a9f54772df
Dominik Schmid <dominikschmid93@gmail.com>
2021-07-31 09:05:44 +1200

    Fix deprecation on `justify` and `createMuiTheme` (#131)
    * fix deprecation of justify
    react.development.js:220 Warning: Failed prop type:
    The prop `justify` of `ForwardRef(Grid)` is deprecated. Use `justifyContent` instead, the prop was renamed.
    
    * Fix deprecation on createMuiTheme.
    
    * Imported from material-ui/core/themes
    
    * actual right import
    
    * Fix CSS for browser source link.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -61,8 +61,7 @@ const useStyles = makeStyles(() =>
       marginTop: '40px',
     },
     browserSource: {
-      position: 'absolute',
-      left: '10px',
+      flexGrow: 1,
     },
   })
 );
@@ -251,7 +250,7 @@ export default function Home() {
                   autoFocus
                 />
               </Grid>
-              <Grid container item xs={12} justify="flex-end">
+              <Grid container item xs={12} justify-content="flex-end">
                 <Button
                   id="send-text"
                   variant="contained"
@@ -274,10 +273,10 @@ export default function Home() {
               container
               spacing={3}
               alignContent="flex-end"
-              justify="flex-end"
+              justify-content="flex-end"
               className={classes.bottomButtons}
             >
-              {/* <Grid container item justify="flex-end" xs={12}> */}
+              {/* <Grid container item justify-content="flex-end" xs={12}> */}
               <div className={classes.browserSource}>
                 Browser source running at:{' '}
                 <a
