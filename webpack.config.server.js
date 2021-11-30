a450f4cb1c04228cee8eaa355f2aabf75c6529c5
omgitsmoe <60219950+omgitsmoe@users.noreply.github.com>
2021-11-30 02:27:25 +0100

    Fix packaged app missing dependencies for the server
    Could not find an obvious way to share common dependencies between main and server
    using webpack with them being in two separate config files
    So the server just gets its owen dependencies bundled

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
-const nodeExternals = require('webpack-node-externals');
 const CopyPlugin = require('copy-webpack-plugin');
 
 const serverConfig = {
@@ -15,18 +14,30 @@ const serverConfig = {
     path: path.resolve(__dirname, 'assets/dist'),
     filename: '[name].js',
   },
-  externals: [nodeExternals()],
+
+  // this works but bundles all the dependencies (with dupes) into server.js
+  // we have to do this, since I could not find a way to share dependencies
+  // between the main electron bundle and the forked process that is the
+  // server (this config)
+  // we could write common deps into a separate module/"dll" but I don't know
+  // how to do that using webpack (in separate configs otherwise we could
+  // use multiple entries with dependOn a shared module) and since the file is
+  // local anyways the gains would be minor (~1MiB size + the load-up of that)
+  externals: [],
   resolve: {
-    extensions: ['.ts', '.tsx'],
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    modules: [path.join(__dirname, 'src/server'), 'node_modules'],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         loader: 'babel-loader',
+        exclude: /node_modules/,
       },
     ],
   },
+
   target: 'node',
   node: {
     __dirname: false,
